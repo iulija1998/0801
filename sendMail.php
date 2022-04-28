ffa0a85df646512fd0c0331d2db83758a00f1e24
Yulia Bogolyubova <103098604+iulija1998@users.noreply.github.com>
2022-04-28 11:59:50 +0300

    sendMail.php

---
+++ sendMail.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+//$userName = $_GET['user_name'];
+$userName = $_POST['user_name'];
+
+//mail('iulija_1998@mail.ru', 'Тестовое письмо', "Привет, $userName");
+
+echo "Hello! $userName";
