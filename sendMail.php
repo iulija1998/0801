47d5d96991dee189b70a9c7be2cfc18d9a8795a3
Yulia Bogolyubova <103098604+iulija1998@users.noreply.github.com>
2022-05-12 19:44:10 +0300

    Delete sendMail.php

--- sendMail.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-
-//$userName = $_GET['user_name'];
-$userName = $_POST['user_name'];
-
-//mail('iulija_1998@mail.ru', 'Тестовое письмо', "Привет, $userName");
-
-echo "Hello! $userName";
